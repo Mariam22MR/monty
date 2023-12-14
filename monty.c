808b5b9043d5b3b332b514ae4f0de62ef405f33d
Mariam22MR <138938363+Mariam22MR@users.noreply.github.com>
2023-12-15 02:41:46 +0300

    Update monty.c

--- monty.c
+++ monty.c
@@ -1,11 +1,15 @@
 #include "main.h"
 stack_t *head = NULL;
 
-/**
- * monty_main - entry point
- * @argc: arguments count
- * @argv: list of arguments
- * Return: always 0
+/*
+ * monty_main - Main function for a Monty interpreter.
+ * @argc: Number of command line arguments.
+ * @argv: Array of command line arguments.
+ *
+ * This function serves as the entry point for the Monty interpreter.
+ * It checks the number of command line arguments and exits with an error
+ * message if not provided correctly. Opens the specified file and performs
+ * cleanup through monty_free() before returning 0.
  */
 
 int monty_main(int argc, char *argv[])
@@ -20,10 +24,13 @@ int monty_main(int argc, char *argv[])
 	return (0);
 }
 
-/**
- * monty_create - Creates a node.
- * @n: Number to go inside the node.
- * Return: Upon sucess a pointer to the node. Otherwise NULL.
+/*
+ * monty_create - Creates a new stack node with the given value.
+ * @n: Integer value to be stored in the new node.
+ *
+ * This function dynamically allocates memory for a new stack node, initializes
+ * its fields with the provided value, and returns a pointer to the created node.
+ * Exits with an error message if memory allocation fails (err code 4).
  */
 stack_t *monty_create(int n)
 {
@@ -31,15 +38,20 @@ stack_t *monty_create(int n)
 
 	node = malloc(sizeof(stack_t));
 	if (node == NULL)
-		err(4);
+		handle_error(4);
 	node->next = NULL;
 	node->prev = NULL;
 	node->n = n;
 	return (node);
 }
 
-/**
- * monty_free - Frees nodes in the stack.
+/*
+ * monty_free - Frees the memory allocated for the stack nodes.
+ *
+ * This function iteratively frees the memory allocated for each node in the stack.
+ * It starts from the head of the stack and proceeds until the end, updating the head
+ * pointer and freeing each node. If the stack is already empty (head == NULL), the
+ * function returns without performing any further actions.
  */
 void monty_free(void)
 {
@@ -56,28 +68,32 @@ void monty_free(void)
 	}
 }
 
-
-/**
- * add_to_line - Adds a node to the queue.
- * @new_node: Pointer to the new node.
- * @ln: line number of the opcode.
+/*
+ * add_to_line - Adds a new node to the end of the stack.
+ * @node: Pointer to the new node to be added.
+ * @l: Unused parameter (line number).
+ *
+ * This function adds a new node to the end of the stack. If the stack is
+ * empty (head == NULL), the new node becomes the head. Otherwise, it traverses
+ * the stack to find the last node and appends the new node to it.
+ * Exits with EXIT_FAILURE if the node or *node is NULL.
  */
-void add_to_line(stack_t **new_node, __attribute__((unused))unsigned int ln)
+void add_to_line(stack_t **node, __attribute__((unused))unsigned int l)
 {
 	stack_t *tmp;
 
-	if (new_node == NULL || *new_node == NULL)
+	if (node == NULL || *node == NULL)
 		exit(EXIT_FAILURE);
 	if (head == NULL)
 	{
-		head = *new_node;
+		head = *node;
 		return;
 	}
 	tmp = head;
 	while (tmp->next != NULL)
 		tmp = tmp->next;
 
-	tmp->next = *new_node;
-	(*new_node)->prev = tmp;
+	tmp->next = *node;
+	(*node)->prev = tmp;
 
 }
