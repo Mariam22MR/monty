46e4f3e5f85411b0281aec03ec193b42e7d56da6
Mariam22MR <138938363+Mariam22MR@users.noreply.github.com>
2023-12-15 02:47:39 +0300

    Update monty_fuc.c

--- monty_fuc.c
+++ monty_fuc.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
 
-/**
- * add_nodes - Adds a node to the stack.
- * @node: Pointer to the new node.
- * @i: Interger representing the line number of of the opcode.
+/*
+ * add_nodes - Adds a new node at the beginning of the stack.
+ * @node: Pointer to the new node to be added.
+ * @l: Unused parameter (line number).
+ *
+ * This function adds a new node at the beginning of the stack. If the stack is
+ * empty (head == NULL), the new node becomes the head. Otherwise, it updates
+ * the head pointer, setting it to the new node, and adjusts the next and prev
+ * pointers accordingly.
+ * Exits with EXIT_FAILURE if the node or *node is NULL.
  */
-void add_nodes(stack_t **node, __attribute__((unused))unsigned int i)
+void add_nodes(stack_t **node, __attribute__((unused))unsigned int l)
 {
 	stack_t *tmp;
 
@@ -24,10 +30,13 @@ void add_nodes(stack_t **node, __attribute__((unused))unsigned int i)
 }
 
 
-/**
- * monty_print_add - Adds a node to the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @len_number: line number of  the opcode.
+/*
+ * monty_print_add - Prints the elements of the stack.
+ * @stack: Pointer to the stack to be printed.
+ * @len_number: Unused parameter (line number).
+ *
+ * This function prints the elements of the stack starting from the top.
+ * Exits with EXIT_FAILURE if the stack is NULL.
  */
 void monty_print_add(stack_t **stack, unsigned int len_number)
 {
@@ -44,10 +53,14 @@ void monty_print_add(stack_t **stack, unsigned int len_number)
 	}
 }
 
-/**
- * monty_pop - Adds a node to the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @len_number: Interger representing the line number of of the opcode.
+/*
+ * monty_pop - Removes the top element from the stack.
+ * @stack: Pointer to the stack from which to pop.
+ * @len_number: Line number for error reporting.
+ *
+ * This function removes the top element from the stack. Exits with an error
+ * message (case 7) if the stack is empty. Adjusts pointers accordingly and
+ * frees the memory of the removed node.
  */
 void monty_pop(stack_t **stack, unsigned int len_number)
 {
@@ -63,10 +76,13 @@ void monty_pop(stack_t **stack, unsigned int len_number)
 	free(tmp);
 }
 
-/**
- * monty_print - Prints the top node of the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @len_number: Interger representing the line number of of the opcode.
+/*
+ * monty_print - Prints the top element of the stack.
+ * @stack: Pointer to the stack to be printed.
+ * @len_number: Line number for error reporting.
+ *
+ * This function prints the top element of the stack. Exits with an error
+ * message (case 6) if the stack is empty.
  */
 void monty_print(stack_t **stack, unsigned int len_number)
 {
