3cc67d248e4a3364fe0ea712183122106dda197d
Mariam22MR <138938363+Mariam22MR@users.noreply.github.com>
2023-12-14 22:42:50 +0300

    Update monty_fuc.c

--- monty_fuc.c
+++ monty_fuc.c
@@ -2,38 +2,38 @@
 
 
 /**
- * add_to_stack - Adds a node to the stack.
- * @new_node: Pointer to the new node.
- * @ln: Interger representing the line number of of the opcode.
+ * add_nodes - Adds a node to the stack.
+ * @node: Pointer to the new node.
+ * @i: Interger representing the line number of of the opcode.
  */
-void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
+void add_nodes(stack_t **node, __attribute__((unused))unsigned int i)
 {
 	stack_t *tmp;
 
-	if (new_node == NULL || *new_node == NULL)
+	if (node == NULL || *_node == NULL)
 		exit(EXIT_FAILURE);
 	if (head == NULL)
 	{
-		head = *new_node;
+		head = *node;
 		return;
 	}
 	tmp = head;
-	head = *new_node;
+	head = *node;
 	head->next = tmp;
 	tmp->prev = head;
 }
 
 
 /**
- * print_stack - Adds a node to the stack.
+ * monty_print - Adds a node to the stack.
  * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: line number of  the opcode.
+ * @len_number: line number of  the opcode.
  */
-void print_stack(stack_t **stack, unsigned int line_number)
+void monty_print(stack_t **stack, unsigned int len_number)
 {
 	stack_t *tmp;
 
-	(void) line_number;
+	(void) len_number;
 	if (stack == NULL)
 		exit(EXIT_FAILURE);
 	tmp = *stack;
@@ -45,16 +45,16 @@ void print_stack(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * pop_top - Adds a node to the stack.
+ * monty_pop - Adds a node to the stack.
  * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * @len_number: Interger representing the line number of of the opcode.
  */
-void pop_top(stack_t **stack, unsigned int line_number)
+void monty_pop(stack_t **stack, unsigned int len_number)
 {
 	stack_t *tmp;
 
 	if (stack == NULL || *stack == NULL)
-		more_err(7, line_number);
+		more_handle_error(7, len_number);
 
 	tmp = *stack;
 	*stack = tmp->next;
@@ -64,13 +64,13 @@ void pop_top(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * print_top - Prints the top node of the stack.
+ * monty_print - Prints the top node of the stack.
  * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * @len_number: Interger representing the line number of of the opcode.
  */
-void print_top(stack_t **stack, unsigned int line_number)
+void monty_print(stack_t **stack, unsigned int len_number)
 {
 	if (stack == NULL || *stack == NULL)
-		more_err(6, line_number);
+		more_handle_error(6, len_number);
 	printf("%d\n", (*stack)->n);
 }
