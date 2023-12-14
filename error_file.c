e7cea1a2edf14fa2b41ffd479e2a6d8832cf651a
Mariam22MR <138938363+Mariam22MR@users.noreply.github.com>
2023-12-15 02:32:07 +0300

    Update error_file.c

--- error_file.c
+++ error_file.c
@@ -1,5 +1,25 @@
 #include "monty.h"
-
+/*
+ * handle_error - Function to handle and report errors in a Monty interpreter
+ * @error_code: Integer representing the specific error code
+ * @...: Variable arguments depending on the error_code
+ *
+ * This function takes an error code and variable arguments to handle and report
+ * errors that may occur during the execution of a Monty interpreter. It utilizes
+ * variable arguments to provide additional information based on the error type.
+ *
+ * Error Codes:
+ *   1 - Missing command line argument (USAGE: monty file)
+ *   2 - Unable to open the specified file
+ *   3 - Unknown instruction encountered
+ *   4 - Memory allocation (malloc) failure
+ *   5 - Invalid usage of the "push" instruction (L%d: usage: push integer)
+ *
+ * Additional Information:
+ *   - For error_code 2, the corresponding filename is passed as a variable argument.
+ *   - For error_code 3, the line number and opcode are passed as variable arguments.
+ *   - For error_code 5, the line number is passed as a variable argument.
+ */
 void handle_error(int error_code, ...)
 {
 	va_list argument;
@@ -34,6 +54,15 @@ void handle_error(int error_code, ...)
 	exit(EXIT_FAILURE);
 }
 
+/*
+ * more_handle_error - Handles and reports additional errors in a Monty interpreter.
+ * @error_code: Integer error code representing the type of error.
+ * @...: Variable arguments providing context based on the error code.
+ *
+ * This function extends error handling for specific cases in a Monty interpreter.
+ * Prints error messages based on the error code and additional context provided
+ * as variable arguments. Calls free_nodes() to free memory and exits with EXIT_FAILURE.
+ */
 void more_handle_error(int error_code, ...)
 {
 	va_list argument;
