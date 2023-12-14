7ac9b8762b26161ab538914e2cc77e18589fbc6e
Mariam22MR <138938363+Mariam22MR@users.noreply.github.com>
2023-12-15 02:55:30 +0300

    Update monty_fuc1.c

--- monty_fuc1.c
+++ monty_fuc1.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
-/**
+/*
  * monty_nop - Does nothing.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @len_number: Interger representing the line number of of the opcode.
+ * @stack: Unused parameter (pointer to the stack).
+ * @len_number: Unused parameter (line number).
+ *
+ * This function does nothing and serves as a placeholder for the "nop" opcode.
+ * It takes no action and is used to maintain proper opcode handling.
  */
 void monty_nop(stack_t **stack, unsigned int len_number)
 {
@@ -11,10 +14,13 @@ void monty_nop(stack_t **stack, unsigned int len_number)
 	(void)len_number;
 }
 
-/**
- * swap_tow_elements - Swaps the top two elements of the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @len_number: Interger representing the line number of of the opcode.
+/*
+ * swap_two_element - Swaps the top two elements of the stack.
+ * @stack: Pointer to the stack to be modified.
+ * @len_number: Line number for error reporting.
+ *
+ * This function swaps the top two elements of the stack. Exits with an error
+ * message (case 8) if the stack is too short (less than two elements).
  */
 void swap_tow_element(stack_t **stack, unsigned int len_number)
 {
@@ -32,12 +38,16 @@ void swap_tow_element(stack_t **stack, unsigned int len_number)
 	*stack = tmp;
 }
 
-/**
- * add_tow_elements - Adds the top two elements of the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @len_number: Interger representing the line number of of the opcode.
+/*
+ * add_two_elements - Adds the top two elements of the stack.
+ * @stack: Pointer to the stack to be modified.
+ * @len_number: Line number for error reporting.
+ *
+ * This function adds the top two elements of the stack and replaces them with
+ * the result. Exits with an error message (case 8) if the stack is too short
+ * (less than two elements).
  */
-void add_nodes(stack_t **stack, unsigned int len_number)
+void add_tow_elements(stack_t **stack, unsigned int len_number)
 {
 	int i;
 
@@ -51,10 +61,14 @@ void add_nodes(stack_t **stack, unsigned int len_number)
 	(*stack)->prev = NULL;
 }
 
-/**
- * sub_tow_elements - Adds the top two elements of the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @len_number: Interger representing the line number of of the opcode.
+/*
+ * sub_two_elements - Subtracts the top element from the second element in the stack.
+ * @stack: Pointer to the stack to be modified.
+ * @len_number: Line number for error reporting.
+ *
+ * This function subtracts the top element from the second element in the stack and
+ * replaces them with the result. Exits with an error message (case 8) if the stack
+ * is too short (less than two elements).
  */
 void sub_tow_elements(stack_t **stack, unsigned int len_number)
 {
@@ -63,7 +77,6 @@ void sub_tow_elements(stack_t **stack, unsigned int len_number)
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 
 		more_handle_err(8, len_number, "sub");
-
 
 	(*stack) = (*stack)->next;
 	i = (*stack)->n - (*stack)->prev->n;
